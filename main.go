fdce295baad8027db6e06aaee0d46bedae35d1a1
pmbertolotti <101236318+pmbertolotti@users.noreply.github.com>
2022-03-09 22:54:30 -0500

    Update main.go

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 		f := fib()
 
 		w.WriteHeader(http.StatusOK)
-		io.WriteString(w, "Hello World - new!\n")
+		io.WriteString(w, "Hello World!\n")
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
